0a97bc6762f1d31bf7b6d697a13db2ac5a8fe41b
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2023-09-02 18:42:52 +0000

    renamed docker compose file

--- Middlesex-microservice-1/index.js
+++ Middlesex-microservice-1/index.js
@@ -8,29 +8,31 @@ apiConfig.use(cors());
 apiConfig.use(bodyParser.json({limit: "50mb"}));
 apiConfig.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit: 5000}));
 
+#For EKS micro2service.default.svc.cluster.local
+#For dockercompose mymicro2
+var hostname = 
+var hostport = 
 
 ApiFunction = async() =>{
 
 apiConfig.get('/getAll', async(req, res)=>{
 console.log("iiiiiii")
    // const {z} = req.body;
- let data1 = await axios.get('http://micro2service.default.svc.cluster.local:3002/getAll')
-console.log("donekdkjdkjs")
-   console.log(data1.data)
-   res.json({"message":"this"})
+ let data1 = await axios.get(`http://${hostname}:${hostport}/getAll`)
+   res.json(data1.data)
 })
  
 apiConfig.get('/get/:id', async(req, res)=>{
    // const {z} = req.body;
    const itemId = parseInt(req.params.id);
-   let data1 = await axios.get('http://mymicro2:3002/get/'+itemId)
+   let data1 = await axios.get(`http://${hostname}:${hostport}/get/`+itemId)
    console.log(data1.data)
    res.json(data1.data)
 })
 
 apiConfig.post('/post', async(req, res)=>{
    // const {z} = req.body;
-   let data1 = await axios.post('http://mymicro2:3002/post',req.body)
+   let data1 = await axios.post(`http://${hostname}:${hostport}/post`,req.body)
    res.json(data1.data)
 
 })
@@ -40,7 +42,7 @@ apiConfig.put('/update/:id', async(req, res)=>{
    // const {z} = req.body;
    const itemId = parseInt(req.params.id);
    const updatedItem = req.body;
-   let data1 = await axios.put('http://mymicro2:3002/update/'+itemId,updatedItem)
+   let data1 = await axios.put(`http://${hostname}:${hostport}/update/`+itemId,updatedItem)
    res.json(data1.data)
 })
 
@@ -48,7 +50,7 @@ apiConfig.put('/update/:id', async(req, res)=>{
 apiConfig.delete('/delete/:id', async(req, res)=>{
     //const {z} = req.body;
     const itemId = parseInt(req.params.id);
-    let data1 = await axios.delete('http://mymicro2:3002/delete/'+itemId)
+    let data1 = await axios.delete(`http://${hostname}:${hostport}/delete/`+itemId)
     res.json(data1.data)
 })
 
